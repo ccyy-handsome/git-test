ec1cb4cad33381dd6678089ea0fb137e1be2bfbe
chenyao <chenyao@xwsoft.com.cn>
2020-10-15 13:43:51 +0800

    把工程添加到本地仓库

---
+++ git-test/src/main/java/com/itheima/FIrstClass.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+/**
+ * @author tutu
+ * @date 2020/10/15 13:37
+ */
+public class FIrstClass {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
