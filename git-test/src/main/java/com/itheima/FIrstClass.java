d03980f693aff80bc20c6ee1047278aef307bf55
chenyao <chenyao@xwsoft.com.cn>
2020-10-15 13:46:26 +0800

    一次修改

--- git-test/src/main/java/com/itheima/FIrstClass.java
+++ git-test/src/main/java/com/itheima/FIrstClass.java
@@ -8,4 +8,8 @@ public class FIrstClass {
     public static void main(String[] args) {
         System.out.println("hello,git");
     }
+
+    public void test(){
+        System.out.println("一次修改");
+    }
 }
